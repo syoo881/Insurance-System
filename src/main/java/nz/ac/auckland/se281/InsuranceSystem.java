371d7f87b59ba5622fb5b1d4a3da97c77c7e59de
Seth Yoo <syoo881@aucklanduni.ac.nz>
2023-03-18 23:40:53 +1300

    Finished create-profile method, and print-profile method. But still need to make username Titlecase

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -5,18 +5,43 @@ import java.util.Arrays;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  private ArrayList userNameList = new ArrayList();
+  private ArrayList ageList = new ArrayList();
+  // I'm not sure if this is supposed to be in here or the Class below (public insurnace system)
+  // Might only need one arraylist that takes information of both
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
+  public String TitleCase(String unmodifieduserName) {
+    String modifieduserName =
+        unmodifieduserName.substring(0, 1).toUpperCase()
+            + unmodifieduserName.substring(1).toLowerCase();
+    return modifieduserName;
+  }
+
   public void printDatabase() {
     // TODO: Complete this method.
+
+    int numberofProfiles = userNameList.size();
+
+    if (numberofProfiles == 0) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+    } else if (numberofProfiles == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), ":");
+      System.out.println("1: " + TitleCase((String) userNameList.get(0)) + ", " + ageList.get(0));
+      // "1: Jordan, 21"
+
+    } else {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), "s", ":");
+      for (int i = 0; i < userNameList.size(); i++) {
+        System.out.println(
+            i + 1 + ": " + TitleCase((String) userNameList.get(i)) + ", " + ageList.get(i));
+      }
+    }
   }
 
   public void createNewProfile(String userName, String age) {
-
-    ArrayList userNameList = new ArrayList();
-    ArrayList ageList = new ArrayList();
 
     try {
       // put fully completed if statement here
@@ -27,6 +52,10 @@ public class InsuranceSystem {
         // Need to make a way to filter out duplicate strings
       } else if ((Integer.parseInt(age) < 0)) {
         MessageCli.INVALID_AGE.printMessage(age, userName);
+      } else {
+        userNameList.add(userName);
+        ageList.add(age);
+        MessageCli.PROFILE_CREATED.printMessage(userName, age);
       }
 
     } catch (NumberFormatException exception) {
