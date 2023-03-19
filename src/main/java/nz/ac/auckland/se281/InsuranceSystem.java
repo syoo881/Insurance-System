663ac5b77bd89177b8ee44e80ad6cc949294d5a1
Seth Yoo <syoo881@aucklanduni.ac.nz>
2023-03-19 23:27:36 +1300

    Updated to make the username titlecase when inputted into the Insurance System. Also added comments

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -7,17 +7,9 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class InsuranceSystem {
   private ArrayList userNameList = new ArrayList();
   private ArrayList ageList = new ArrayList();
-  // I'm not sure if this is supposed to be in here or the Class below (public insurnace system)
-  // Might only need one arraylist that takes information of both
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-  }
-
-  public String TitleCase(String unmodifieduserName) {
-    String modifieduserName =
-        unmodifieduserName.substring(0, 1).toUpperCase()
-            + unmodifieduserName.substring(1).toLowerCase();
-    return modifieduserName;
   }
 
   public void printDatabase() {
@@ -28,34 +20,37 @@ public class InsuranceSystem {
     if (numberofProfiles == 0) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
     } else if (numberofProfiles == 1) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), ":");
-      System.out.println("1: " + TitleCase((String) userNameList.get(0)) + ", " + ageList.get(0));
-      // "1: Jordan, 21"
-
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), ":", "");
+      System.out.println("1: " + titleCase(userNameList.get(0)) + ", " + ageList.get(0));
     } else {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), "s", ":");
       for (int i = 0; i < userNameList.size(); i++) {
-        System.out.println(
-            i + 1 + ": " + TitleCase((String) userNameList.get(i)) + ", " + ageList.get(i));
+        System.out.println(i + 1 + ": " + titleCase(userNameList.get(i)) + ", " + ageList.get(i));
       }
     }
+  }
+
+  public String titleCase(Object unmodifieduserName) {
+    String strunmodifieduserName = unmodifieduserName.toString();
+    String modifieduserName =
+        strunmodifieduserName.substring(0, 1).toUpperCase()
+            + strunmodifieduserName.substring(1).toLowerCase();
+    return modifieduserName;
   }
 
   public void createNewProfile(String userName, String age) {
 
     try {
-      // put fully completed if statement here
       if (userName.length() < 3) {
         MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
       } else if (Arrays.asList(userNameList).contains(userName)) {
         MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
-        // Need to make a way to filter out duplicate strings
       } else if ((Integer.parseInt(age) < 0)) {
         MessageCli.INVALID_AGE.printMessage(age, userName);
       } else {
         userNameList.add(userName);
         ageList.add(age);
-        MessageCli.PROFILE_CREATED.printMessage(userName, age);
+        MessageCli.PROFILE_CREATED.printMessage(titleCase(userName), age);
       }
 
     } catch (NumberFormatException exception) {
