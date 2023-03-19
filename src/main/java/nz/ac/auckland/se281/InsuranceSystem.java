cbfa0841300d254227e9bab8d0f163d395c4b797
Seth Yoo <syoo881@aucklanduni.ac.nz>
2023-03-19 23:31:12 +1300

    Added indentations

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -13,17 +13,16 @@ public class InsuranceSystem {
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
-
-    int numberofProfiles = userNameList.size();
-
-    if (numberofProfiles == 0) {
+    int numberOfProfiles = userNameList.size();
+    // If statement for three separate cases - when numberOfProfiles=0, = 1, > 1
+    // Using the MessageCli file to print the appropriate message for each case
+    if (numberOfProfiles == 0) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
-    } else if (numberofProfiles == 1) {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), ":", "");
+    } else if (numberOfProfiles == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberOfProfiles), ":", "");
       System.out.println("1: " + titleCase(userNameList.get(0)) + ", " + ageList.get(0));
     } else {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberofProfiles), "s", ":");
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(numberOfProfiles), "s", ":");
       for (int i = 0; i < userNameList.size(); i++) {
         System.out.println(i + 1 + ": " + titleCase(userNameList.get(i)) + ", " + ageList.get(i));
       }
@@ -31,15 +30,17 @@ public class InsuranceSystem {
   }
 
   public String titleCase(Object unmodifieduserName) {
-    String strunmodifieduserName = unmodifieduserName.toString();
-    String modifieduserName =
-        strunmodifieduserName.substring(0, 1).toUpperCase()
-            + strunmodifieduserName.substring(1).toLowerCase();
-    return modifieduserName;
+    // Converting the Object input into a string.
+    String stringUnmodifiedUserName = unmodifieduserName.toString();
+    // Converting string to lowercase, and uppercasing only the first letter in one line of code.
+    String modifiedUserName =
+        stringUnmodifiedUserName.substring(0, 1).toUpperCase()
+            + stringUnmodifiedUserName.substring(1).toLowerCase();
+    return modifiedUserName;
   }
 
   public void createNewProfile(String userName, String age) {
-
+    // Using try-catch to remove the error when string is inserted into age integer.
     try {
       if (userName.length() < 3) {
         MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
@@ -48,16 +49,14 @@ public class InsuranceSystem {
       } else if ((Integer.parseInt(age) < 0)) {
         MessageCli.INVALID_AGE.printMessage(age, userName);
       } else {
+        // Only adding username and age to array if they are valid.
         userNameList.add(userName);
         ageList.add(age);
         MessageCli.PROFILE_CREATED.printMessage(titleCase(userName), age);
       }
-
     } catch (NumberFormatException exception) {
       MessageCli.INVALID_AGE.printMessage(age, userName);
     }
-    // TODO: Complete this method.
-
   }
 
   public void loadProfile(String userName) {
